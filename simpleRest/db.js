4f56a79212fde5f644e7c89631b774870313ba4a
AaloCat <lauloplay@gmail.com>
2023-10-19 12:58:32 +0200

    Updated something

--- simpleRest/db.js
+++ simpleRest/db.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql2');
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 20,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 20,
 });
 
 module.exports = {
-    query: (text, params, callback) => pool.query(text, params, callback),
+  query: (text, params, callback) => pool.query(text, params, callback),
 };
--- simpleRest/index.js
+++ simpleRest/index.js
@@ -1,52 +1,72 @@
-const express = require("express");
+// app.js
+const express = require('express');
 const app = express();
 const port = 3010;
 const db = require('./db');
 app.use(express.json());
 
 const flavors = [
-    {
-        name: "Schokolade"
-    },
-    {
-        name: "Vanille"
-    },
-    {
-        name: "Zitrone"
-    },
-    ];
+  {
+    name: 'Schoko',
+  },
+  {
+    name: 'Vanille',
+  },
+  {
+    name: 'Zitrone',
+  },
+];
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-app.post("/flavor/:id", async (req, res) => {
-    try{
-        const id = req.params.id;
-        console.log(`id: ${id}`);
-        res.send("flavor updated");        
-    }
-    catch{
-        res.status(402).send("Invalid request");
-    }
+app.post('/flavor/:id', async (req, res) => {
+  const id = req.params.id;
+  console.log(`ID from URL: ${id}`);
+
+  res.send('flavor updated');
 });
 
 app.post('/ersetzeEis/:index', (req, res) => {
-    const index = parseInt(req.params.index);
-    const neuesEis = req.body.neuesEis;
+  const index = parseInt(req.params.index);
+  const neuesEis = req.body.neuesEis;
 
-    if (flavors[index] !== undefined && neuesEis) {
-        flavors[index].name = neuesEis;
-        res.send("Erfolgreich ersetzt!");
-    } else {
-        res.status(400).send("Ungültige Anfrage");
-    }
+  if (flavors[index] !== undefined && neuesEis) {
+    flavors[index].name = neuesEis;
+    res.send('Erfolgreich ersetzt!');
+  } else {
+    res.status(400).send('Ungültige Anfrage');
+  }
 });
 
 app.get('/flavors', (req, res) => {
-    res.json(flavors);
+  res.json(flavors);
 });
 
+// Get flavor with id
+app.get('/flavors/:id', async (req, res) => {
+  try {
+    // read parameter from url
+    const id = req.params.id;
+
+    // execute query
+    db.query('SELECT * FROM flavors WHERE id = ?', [id], (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'Failed to fetch flavor' });
+      }
+      res.status(200).json(result);
+    });
+    // db.query('SELECT * FROM flavors', '', (err, result) => {
+    //   console.log('Error: ', err);
+    //   console.log('Result: ', result);
+    // });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch flavor' + err });
+    console.log(err);
+  }
+});
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/}`);
+  console.log(`Server running at http://localhost:${port}/}`);
 });
