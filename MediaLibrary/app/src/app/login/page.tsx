ce58499250d4ce181b3106428585b133210ee25f
AaloCat <lauloplay@gmail.com>
2023-11-21 09:23:12 +0100

    small changes

---
+++ MediaLibrary/app/src/app/login/page.tsx
@@ -0,0 +1,5 @@
+import styles from './page.module.css';
+
+export default function Login() {
+  return <div>Login in to use the App</div>;
+}
--- MediaLibrary/app/src/app/page.tsx
+++ MediaLibrary/app/src/app/page.tsx
@@ -1,5 +1,12 @@
+import Link from 'next/link';
 import styles from './page.module.css';
 
 export default function Home() {
-  return <main className={styles.main}>Landing Page</main>;
+  return (
+    <main className={styles.main}>
+      <h1>
+        Landing Page <Link href='/about'>About</Link>
+      </h1>
+    </main>
+  );
 }
