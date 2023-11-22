14c5e455702ac726c1010c9b492a657e2e35e5dd
Laurin <101341618+AaloFanaa@users.noreply.github.com>
2023-11-22 21:07:15 +0100

    fixed the login and everything is working

--- ScreenTale/app/src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './login.module.css';
-
-export default function Login() {
-  return (
-    <div className={styles.wrapper}>
-      <div>Login with Google</div>
-    </div>
-  );
-}
--- ScreenTale/app/src/app/page.tsx
+++ ScreenTale/app/src/app/page.tsx
@@ -1,14 +1,9 @@
 import Link from 'next/link';
 import styles from './page.module.css';
-import Header from './header';
-import { useSession } from 'next-auth/react';
 
 export default async function App() {
-  const { data: session } = useSession();
-
   return (
     <>
-      <Header></Header>
       <div className={styles.wrapper}>
         <h1>Main Page</h1>
         <Link href='/login'>Go to login</Link>
