b9aaf2f180d8f662c1ca4cd40febe4259f152f46
sveniiboyy <119489548+sveniiboyy@users.noreply.github.com>
2023-11-23 12:45:18 +0100

    Modal

--- ScreenTale/app/src/app/page.tsx
+++ ScreenTale/app/src/app/page.tsx
@@ -1,10 +1,40 @@
+'use client';
+
 import Link from 'next/link';
 import styles from './page.module.css';
+import ModalDialog from './components/modalDialog';
+import { useState } from 'react';
 
 export default async function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const modalStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
   return (
     <>
-      <div className={styles.wrapper}></div>
+      <div className={styles.wrapper}>
+        <button onClick={openModal}>Open Modal</button>
+      </div>
+      <ModalDialog isOpen={modalIsOpen} onClose={closeModal} customStyles={}>
+        <h2>Modal Content</h2>
+        <p>This is the content of the modal.</p>
+      </ModalDialog>
     </>
   );
 }
