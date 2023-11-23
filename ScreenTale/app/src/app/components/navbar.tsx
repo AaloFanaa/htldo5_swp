54deca06cf560f342cd96f5b505258464334a66c
AaloCat <lauloplay@gmail.com>
2023-11-23 12:46:12 +0100

    added menu thingy

--- ScreenTale/app/src/app/components/navbar.tsx
+++ ScreenTale/app/src/app/components/navbar.tsx
@@ -4,32 +4,35 @@ import AuthProvider from '../context/authProvider';
 import styles from './navbar.module.css';
 import { useSession } from 'next-auth/react';
 
+import Image from 'next/image';
+import userIcon from '../../../public/user-icon.svg';
+import movieIcon from '../../../public/movie-icon.svg';
+import bookmarkIcon from '../../../public/bookmark-icon.svg';
+import bookIcon from '../../../public/book-icon.svg';
+
 export default function navbar() {
   const session = useSession();
-  console.log(session);
-  if (!session) {
-    console.log('You should be logged out');
-  }
+
   return (
     <AuthProvider>
       <div className={styles.navbarWrapper}>
-        <div className={styles.navbarMenu}>Menu</div>
-        <div className={styles.navbarUser}>
-          <div className={styles.userName}>
-            {session.data ? session.data?.user?.name : 'Loading...'}
+        <div className={styles.navbarMenu}>
+          <div className={styles.menuIcon}>
+            <Image src={bookIcon} alt='Books' className={styles.iconImage} />
           </div>
+          <div className={styles.menuIcon}>
+            <Image src={movieIcon} alt='Books' className={styles.iconImage} />
+          </div>
+          <div className={styles.menuIcon}>
+            <Image src={bookmarkIcon} alt='Books' className={styles.iconImage} />
+          </div>
+        </div>
+        <div className={styles.navbarUser}>
+          <div className={styles.userName}>{session.data ? session.data?.user?.name : 'Loading...'}</div>
           {session.data?.user?.image ? (
-            <img
-              src={session.data.user?.image}
-              alt='User image'
-              className={styles.userImage}
-            />
+            <img src={session.data.user?.image} alt='User image' className={styles.userImage} />
           ) : (
-            <img
-              src={'./user-icon.svg' as string}
-              alt='User image'
-              className={styles.userImage}
-            />
+            <Image priority src={userIcon} alt='User image' className={styles.userImage} />
           )}
         </div>
       </div>
