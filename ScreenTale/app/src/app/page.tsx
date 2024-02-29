b7768ea3c5e4d387e74259ccb3c5d845e2c42527
AaloCat <lauloplay@gmail.com>
2024-02-29 12:54:52 +0100

    reworked main page

--- ScreenTale/app/src/app/page.tsx
+++ ScreenTale/app/src/app/page.tsx
@@ -9,10 +9,6 @@ import firebase, { initializeApp } from 'firebase/app';
 import 'firebase/firestore';
 import { getFirestore } from 'firebase/firestore';
 
-const bestsellerUrl =
-  'https://api.nytimes.com/svc/books/v3/lists.json?list=hardcover-fiction&api-key=qkQgmC5hGtFlpt5A9BvWA09S2zAu6SUE';
-// const bestsellerUrl = 'AAAAA';
-
 const firebaseConfig = {
   apiKey: 'AIzaSyC3ZyjlEmP3yUoPQbGq7-A7p6Eu4-lDCtY',
   authDomain: 'screentale-ddc79.firebaseapp.com',
@@ -25,10 +21,21 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
 
+type Movie = {
+  title: string;
+  image: string | null;
+  year: string | null;
+  link: string;
+};
+
 export default function App() {
   const { activePage, setActivePage } = useActivePage();
-  const [bookArray, setBookArray] = useState<Array<any> | null>(null);
+  const [entryArray, setEntryArray] = useState<Array<Movie>>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const getSearchUrl: () => string = () => {
+    return 'http://www.omdbapi.com/?apikey=b93d24e3&s=random&type=movie';
+  };
 
   useEffect(() => {
     setActivePage(0);
@@ -37,14 +44,29 @@ export default function App() {
       setIsLoading(true);
       console.log('Fetching data...');
       try {
-        const response = await fetch(bestsellerUrl);
+        const response = await fetch('http://www.omdbapi.com/?apikey=b93d24e3&s=random&type=movie');
         if (!response.ok) {
           throw new Error('Failed to fetch data from the API');
         }
         const result: any = await response.json();
-        setBookArray(await result.results);
+        if (result.Response === 'False') {
+          return;
+        }
+        if (result.Search.length === 0) {
+          return;
+        }
+        result.Search.forEach((res: any, i: number) => {
+          console.log(res);
+          let newEntry: Movie = {
+            title: res.Title,
+            image: res.Poster,
+            link: `http://www.omdbapi.com/?apikey=b93d24e3&t=${res.Title}`,
+            year: res.Year,
+          };
+          setEntryArray((entryArray) => [...entryArray, newEntry]);
+        });
       } catch (error) {
-        console.log('Error fetching data', error);
+        console.error('Error fetching data', error);
       } finally {
         setIsLoading(false);
         console.log('Done fetching!');
@@ -62,14 +84,14 @@ export default function App() {
       ) : (
         <></>
       )}
-      {bookArray?.map((book) => {
+      {entryArray?.map((entry) => {
         return (
           <DisplayCard
             onDelete={() => {}}
-            displayName={book.book_details[0].title}
-            info={book.book_details[0].author}
-            image={null}
-            link={'1233'}
+            displayName={entry.title}
+            info={entry.year}
+            image={entry.image}
+            link={entry.link}
             showDelButton={false}
             showAddButton={true}></DisplayCard>
         );
